0e4451c0e49dac7124a55f899ba5f70dfe1a606f
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2017-08-16 12:57:28 +0000

    Add script to load bl module

---
+++ testing/auth_test/load_new_bl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+### users-bl module
+echo "Registering the Users-BL module"
+curl -w '\n' -X POST -D - \
+    -H "Content-type: application/json" \
+    -d @./mod-users-bl/ModuleDescriptor.json \
+    http://localhost:9130/_/proxy/modules
+
+echo "Deploying the Users-BL module"
+curl -w '\n' -D - -s \
+    -X POST \
+    -H "Content-type: application/json" \
+    -d @./deployment_descriptors/users-bl_new.json \
+    http://localhost:9130/_/discovery/modules
+
+echo "Adding the Users-BL module to our tenant"
+curl -w '\n' -X POST -D - \
+    -H "Content-type: application/json" \
+    -d @./tenant_associations/users-bl.json \
+    http://localhost:9130/_/proxy/tenants/diku/modules
+
+
